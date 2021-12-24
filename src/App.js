50a40e63198d4fedc84e1e5eb28d803dff004f49
MohammedBakr44 <mohammed.mobile.tricks@gmail.com>
2021-12-24 15:22:47 +0200

    Added the state as a dependency for useEffect so the changes happen without needing a reload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import * as BooksAPI from "./BooksAPI";
+import Search from "./Search";
 import "./App.css";
 
 import Book from "./Book";
@@ -7,30 +8,14 @@ import Book from "./Book";
 const BooksApp = () => {
   const [books, addBooks] = useState([]);
 
-  useEffect(() => {
-    BooksAPI.getAll().then((data) => addBooks([...data]));
-  }, []);
+  useEffect(
+    () => {
+      BooksAPI.getAll().then((data) => addBooks([...data]));
+    },
+    [books]
+  );
   return (
     <div className="app">
-      <div className="search-books">
-        <div className="search-books-bar">
-          <button className="close-search">Close</button>
-          <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <input type="text" placeholder="Search by title or author" />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid" />
-        </div>
-      </div>
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
